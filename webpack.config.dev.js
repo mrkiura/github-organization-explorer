716ca584dc48e0bef82af739814d4cf3083a08b5
Alex Kiura <kiuraalex@gmail.com>
2021-03-02 09:04:10 +0300

    Webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,25 +1,111 @@
-module.exports = {
-    devtool: 'inline-source-map',
-    entry: './src/index.js',
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = options => ({
+    mode: options.mode,
+    entry: options.entry,
     output: {
-      path: __dirname + '/dist',
-      publicPath: '/',
-      filename: 'bundle.js'
+        // Compile into js/build.js
+        path: path.resolve(process.cwd(), 'build'),
+        publicPath: '/',
+
+        // Merge with env dependent settings
+        ...options.output,
     },
-    devServer: {
-      contentBase: './dist',
-    },
+    optimization: options.optimization,
     module: {
-      rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader']
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
-      },
-      ]
+        rules: [
+            {
+                test: /\.(js|jsx)$/, // Transform all .js and .jsx files required somewhere with Babel
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                // Preprocess our own .css files
+                // This is the place to add your own loaders (e.g. sass/less etc.)
+                // for a list of loaders, see https://webpack.js.org/loaders/#styling
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                // Preprocess 3rd party .css files located in node_modules
+                test: /\.css$/,
+                include: /node_modules/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(eot|otf|ttf|woff|woff2)$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            // Inline files smaller than 10 kB
+                            limit: 10 * 1024,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            // Inline files smaller than 10 kB
+                            limit: 10 * 1024,
+                        },
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                enabled: false,
+                                // NOTE: mozjpeg is disabled as it causes errors in some Linux environments
+                                // Try enabling it in your environment by switching the config to:
+                                // enabled: true,
+                                // progressive: true,
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            optipng: {
+                                optimizationLevel: 7,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.9],
+                                speed: 4,
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader',
+            },
+            {
+                test: /\.(mp4|webm)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                    },
+                },
+            },
+        ],
     },
-  };
+    resolve: {
+        modules: ['node_modules', 'app'],
+        extensions: ['.js', '.jsx', '.react.js'],
+    },
+    devtool: options.devtool,
+    target: 'web', // Make web variables accessible to webpack, e.g. window
+    performance: options.performance || {},
+});
