7f2a226364d45cc8e031bf2be9b69367c7706eea
Alex Kiura <kiuraalex@gmail.com>
2021-03-03 01:19:27 +0300

    Add component for displaying repos for a contributor

---
+++ src/components/NestedRepositoriesList.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import {
+    InputGroup,
+    InputGroupText,
+    InputGroupAddon,
+    Input,
+    Table,
+    Container,
+    Row,
+    Col,
+} from 'reactstrap';
+import { usePaginate } from '../hooks/usePaginate';
+import { usePageInfo } from '../hooks/usePageInfo';
+import { useSortData } from '../hooks/useSortData';
+import { RepositoryRow } from './RepositoryRow';
+import { Paginator } from './Paginator';
+import { getClassNamesFor } from '../utils';
+
+
+export const NestedRepositoriesList = ({ repositories }) => {
+    const { getPageInfo, setPageInfo } = usePageInfo();
+    let { selectedPage, pageLimit, pageCount } = getPageInfo();
+    pageCount = Math.ceil(repositories.length / pageLimit);
+    const { page } = usePaginate(repositories, pageLimit, selectedPage);
+
+    const { sortedData, sortConfig } = useSortData(page);
+
+    return (
+        <Container>
+            <div className="center">
+                <Row className="justify-content-between">
+                    <Table hover>
+                        <thead className="profile-info">
+                            <tr className="left">
+                                <th className={getClassNamesFor('repositories', sortConfig)}>
+                                    Repository
+                                </th>
+                                <th className={getClassNamesFor('description', sortConfig)}>
+                                    Description
+                                </th>
+                                <th className={getClassNamesFor('language', sortConfig)}>
+                                    Language
+                                </th>
+                                <th className={getClassNamesFor('forks', sortConfig)}>Forks</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {sortedData.map((repo, index) => (
+                                <RepositoryRow repo={repo} key={index}/>
+                            ))}
+                        </tbody>
+                    </Table>
+                </Row>
+                <Row>
+                    <Col sm="12" md={{ size: 6, offset: 3 }}>
+                        <Paginator
+                            pageCount={pageCount}
+                            onPageChange={(data) => {
+                                setPageInfo(data.selected + 1, pageCount);
+                            }}
+                        />
+                    </Col>
+                </Row>
+            </div>
+        </Container>
+    );
+};
